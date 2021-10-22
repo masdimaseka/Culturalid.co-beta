2ca8b2556511c892a9725148e88030ee984e6f65
DimasEka15 <dimasekaid@gmail.com>
2021-10-22 20:45:52 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const bg = document.querySelector(".main-bg");
+
+function printImage(imgName) {
+  bg.setAttribute("style", `background-image: url(../img/bg/${imgName}.png)`);
+}
+
+function getImgName() {
+  let imgId = 0;
+  const listImg = ["tanahLot", "barong"];
+  //   console.log(listImg.length);
+  setInterval(() => {
+    if (imgId < listImg.length) {
+      let imgName = listImg[imgId];
+      imgId += 1;
+      printImage(imgName);
+    } else {
+      imgId = 0;
+    }
+  }, 3000);
+}
+
+getImgName();
